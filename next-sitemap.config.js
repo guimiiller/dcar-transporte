35319490ec12957c9303f650e06c2256abe8570b
guimiiller <guilhermemillerblack@gmail.com>
2025-02-23 11:21:50 -0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://dcartransporte.com.br', // Troque pelo seu domínio
+    generateRobotsTxt: true, // Cria também um robots.txt
+    sitemapSize: 7000, // Número máximo de URLs por arquivo sitemap
+};
+  
